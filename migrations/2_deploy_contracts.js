03e473a8d31364258309c378d9ca5c9fe5356c71
Carlos Gonzalez <carlosgj94@gmail.com>
2017-12-08 20:49:02 +0100

    Added the event smart contract

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,9 +1,9 @@
-var Users = artifacts.require("./Users.sol");
 var Guess = artifacts.require("./Guess.sol");
 var User = artifacts.require("./User.sol");
+var Event = artifacts.require("./Event.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(Users);
   deployer.deploy(User);
   deployer.deploy(Guess);
+  deployer.deploy(Event);
 };
