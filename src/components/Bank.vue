7cb84c7ccdbdc491b73040a922ca86a034bdec3a
Carlos Gonzalez <carlosgj94@gmail.com>
2017-12-17 00:44:44 +0100

    You can see your address and the qr in the bank view

--- src/components/Bank.vue
+++ src/components/Bank.vue
@@ -4,6 +4,19 @@
       <md-avatar class="md-large qr">
           <qrcode :foreground="foreground" :background="background" :size="size" :cls="qrCls" :value="address"></qrcode>
       </md-avatar>
+      <md-divider class='space'></md-divider>
+      <h3>Receive</h3>
+      <small>{{address}}</small>
+      <h3>Send</h3>
+      <md-field>
+      <label>Address</label>
+      <md-input v-model="initial"></md-input>
+    </md-field>
+
+    <md-field>
+      <label>Value</label>
+      <md-input v-model="initial"></md-input>
+    </md-field>
   <!-- <Chest/> -->
   </div>
 </template>
@@ -48,5 +61,9 @@ export default{
     width: 120px !important;
     height: 120px !important;
     border-radius: 5px !important;
+}
+
+.space{
+    margin-top: 15px;
 }
 </style>
