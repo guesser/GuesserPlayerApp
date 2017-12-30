32670e49b9911b7ce19d80d8279d89a04a56623c
Carlos Gonzalez <carlosgj94@gmail.com>
2017-12-30 21:17:12 +0100

    Final test of the guess contract

--- test/truffle/TestGuess.sol
+++ test/truffle/TestGuess.sol
@@ -12,4 +12,25 @@ contract TestGuess {
     uint256 length = guessObject1.getGuessesLength();
     Assert.equal(length, 0, "Number of guesses is not 0.");
   }
+
+  function testCreateGuess() public {
+    bytes32 _t = "hola";
+    bytes32 _d = "holahola";
+    bytes32 _tc = "crypto";
+    address _c = msg.sender;
+    bytes32 _eV = 30;
+    bytes32 _o1 = "yes";
+    bytes32 _o2 = "no";
+    bytes32 _o3 = "maybe";
+    bytes32 _o4 = "no way";
+
+    guessObject.setGuess(_t, _d, _tc, _c, _eV, _o1, _o2, _o3, _o4);
+    uint256 length = guessObject.getGuessesLength();
+    Assert.equal(length, 1, "Number of guesses is not 1");
+  }
+
+  function testGetGuess() public {
+    var (_t, _d, _to, _c, _eV, _da) = guessObject.getGuess(0);
+    Assert.equal(_t, "hola", "The first item is not hola");
+  }
 }
