bb62c829113ab623abcc4cc7f8df684745391c1e
BorjaIP <borja.irpe@gmail.com>
2018-03-22 19:49:15 +0100

    Responsive text size Landing page

--- src/components/Landing.vue
+++ src/components/Landing.vue
@@ -7,13 +7,13 @@
         <b-row class="maket" align-h="center">
           <b-container class="" style="padding-top: 10%">
             <b-row align-h="between" style="">
-              <b-col lg="8" align-self="end" style="margin-right: 5%;">
-                <h1>Can you predict the outcome</h1>
-                <h1>of the internet's hottest events?</h1>
-                <h2 style="color: gray;">Prove it</h2>
+              <b-col lg="8" align-self="end" style="text-align: left; margin-right: 5%;">
+                <h1 style="font-size: calc(1em + 2vw);">Can you predict the outcome</h1>
+                <h1 style="font-size: calc(1em + 2vw);"> of the internet's hottest events?</h1>
+                <h2 style="font-size: calc(1em + 1.5vw); color: gray;">Prove it</h2>
               </b-col>
-              <b-col style="">
-                <b-container class="maket" style="padding: 2%; justify-content: center;">
+              <b-col>
+                <b-container class="maket" style="padding: 2%; text-align: center;">
                   <img src="../assets/magic-ball2.svg"
                        style="width: 100%; max-width: 30vw;"/>
                 </b-container>
@@ -35,9 +35,9 @@
                      style="width: 35%; height: 100%; max-width: 40vh;"/>
               </b-container>
             </b-row>
-            <h1>What is Guesser?</h1>
+            <h1 style="font-size: calc(1em + 2vw);">What is Guesser?</h1>
             <br>
-            <h4>
+            <h4 style="font-size: calc(1em + 0.5vw);">
               Guesser is a prediction game built on top of Ethereum where players stake money among each other based on what outcome they believe will happen at a given event.
               <br>
               Are you a true Guesser?
@@ -51,12 +51,11 @@
     <div class="section">
       <div class="explainer">
         <b-row class="maket" align-h="center">
-          <b-container class="" style="text-align: center">
+          <b-container style="text-align: center;">
             <b-row align-h="between" style="">
             </b-row>
-            <h1>How can I <b>Play</b>?</h1>
-            <br>
-            <h4 style="color: gray;">
+            <h1 style="font-size: calc(1em + 2vw);">How can I <b>Play</b>?</h1>
+            <h4 style="font-size: calc(1em + 0.5vw); padding-top: 5%; color: gray;">
               To make a guess at a popular internet event you only have to choose one of the two outcomes available and select how much <img src="../assets/ethereum.png" class="d-inline-block align-top" style="width: 25px;" alt="|eth|">Ether you are willing to lose if you’re wrong. Guessers that vote the right outcome will get all Ether staked on the event (after fees) distributed proportionally to how much they risked.
             </h4>
           </b-container>
@@ -125,8 +124,9 @@
               <b-col lg="6" align-self="center" style="text-align: right;">
                 <b-row class="justify-content-md-center">
                   <b-col lg="10" style="">
-                    <h1><b>Create</b> an Event!</h1>
-                    <h4 style="color: gray;">
+                    <h1 style="font-size:calc(1em + 2vw);"><b>Create</b> an Event!</h1>
+                    <br>
+                    <h4 style="font-size:calc(1em + 0.5vw); color: gray;">
                       You believe an event will get traction amongst the Guesser community?<br>
                       <br>Create it,<br>choose the possible outcomes and catch players’ attention in order to get a % fee of all the money players send when voting for an outcome.
                     </h4>
@@ -149,10 +149,10 @@
       <div class="explainer">
         <b-row class="maket" align-h="center">
           <b-container class="" style="text-align: center">
-            <h1 style="color: #ff0d73; font-weight: bold; font-size: 50px">Validate</h1>
-            <h1>Who says what really happened?</h1>
+            <h1 style="color: #ff0d73; font-weight: bold; font-size:calc(1em + 3vw);">Validate</h1>
+            <h1 style="font-size:calc(1em + 2vw);">Who says what really happened?</h1>
             <br>
-            <h4>
+            <h4 style="font-size:calc(1em + 0.5vw);">
               Guesser relies on validators staking tokens in order to achieve consensus on what was the true outcome of an event. Validators will be financially rewarded for telling the truth, and punished for lying about a certain result.
             </h4>
           </b-container>
@@ -165,24 +165,22 @@
       <div class="explainer">
         <b-row class="maket" align-h="center">
           <b-container class="maket" style="text-align: center;">
-            <h1>
-              Trust the Beard<img src="../assets/beard.png" class="d-inline-block align-top" style="width: 50px;" alt="BV">
+            <h1 style="font-size:calc(1em + 2vw); margin-top: 5%;">
+              Trust the Beard <img src="../assets/beard.png" class="d-inline-block align-top" style="width: calc(20px + 2vw);" alt="BV">
             </h1>
             <br>
-            <h3 style="color: gray">
+            <h3 style="font-size:calc(1em + 0.75vw); color: gray">
               Guesser is one of the first games built on Ethereum technology.
               <br>We believe in the potential of the blockchain to change the way people have fun predicting the future.
               <br>Harnessing the power of smart contracts, Guesser lets anyone create fun events where others can play freely without trusting any central authority to manage their funds or dictate the truth.
               <br>
               <br>The house can’t always win if there isn’t one in the first place!
             </h3>
-            <b-container style="padding: 2%; margin-top: 10%; display: flex; justify-content: center;">
+            <b-container style="margin-top: 9%; display: flex; justify-content: center;">
               <b-button href="#home" variant="outline-primary lg" style="padding-bottom: 0;">
-                <h1>Start Guessing!</h1>
+                <h1 style="font-size:calc(1em + 2vw);">Start Guessing!</h1>
               </b-button>
             </b-container>
-            <b-row align-h="between" style="">
-            </b-row>
           </b-container>
         </b-row>
       </div>
