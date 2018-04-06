f2c51dcc4a115a2f44f74bc9c629544bbc2b9144
XPi2 <uxiopineiro@gmail.com>
2018-04-07 00:26:54 +0200

    SingleCard component implementation in Search

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -19,54 +19,10 @@
       <Loading/>
     </div>
     <div v-if="guessExists" class='inside' style="">
-      <b-card :border-variant="guess.topic"
-                                           :header="guess.title"
-                                           :header-bg-variant="guess.topic"
-                                           header-text-variant="white"
-                                           class="text-center">
-        <p class="card-text">
-        {{guess.description}}
-        </p>
-        <br>
-         <p class="card-text">
-        Created at: <b>{{guess.startingDay}}</b>
-        <br>
-        Open until: <b>{{guess.finishingDay}}</b>
-        </p>
-        <br>
-        <!--Number of people Progress Bar-->
-        <span>Votes for each outcome: </span>
-        <b-progress class="mt-1" :max="10*(guess.votes/10)" show-value striped>
-          <b-progress-bar :value="10*(guess.option1votes/10)" variant="pink">
-            {{guess.option1}} - {{ guess.option1votes }}
-          </b-progress-bar>
-          <b-progress-bar :value="10*(guess.option2votes/10)" variant="magenta">
-            {{guess.option2}} - {{ guess.option2votes }}
-          </b-progress-bar>
-        </b-progress>
-        <small>Total: {{guess.votes}} people</small>
-
-        <!--Amount of eth in each option-->
-        <br>
-        <br>
-        <span>Eth staked on each outcome: </span>
-        <b-progress class="mt-1" :max="10*(guess.amountEth/10)" show-value striped>
-          <b-progress-bar :value="10*(guess.option1AmountEth/10)" variant="pink">
-            {{guess.option1}} - {{ guess.option1AmountEth }}
-          </b-progress-bar>
-          <b-progress-bar :value="10*(guess.option2AmountEth/10)" variant="magenta">
-            {{guess.option2}} - {{ guess.option2AmountEth }}
-          </b-progress-bar>
-        </b-progress>
-        <small>Total: {{guess.amountEth}} ether</small>
-
-        <br>
-        <br>
-        <br>
-        <b-button style="margin-right: 20px" @click="showPaymentModal(1)" variant="outline-pink" size="sm">{{guess.option1}}</b-button>
-        <b-button @click="showPaymentModal(2)" variant="outline-magenta" size="sm">{{guess.option2}}</b-button>
-      </b-card>
+      <SingleCard :eventItem="guess"/>
     </div>
+
+    <!--If event doesn't exist-->
     <div v-else>
       <b-row align-h="center">
         <img src="static/beard-hold.png" style="width: 30%; height: 30%;" alt=":'("/>         
@@ -101,24 +57,26 @@
 // Import section
 import Loading from '../components/Loading.vue'
 import GuessHelper from '@/js/Guess'
+import SingleCard from './Common/SingleCard.vue'
 
 export default {
   name: 'Search',
   props: ['topic'],
   components: {
-    Loading
+    Loading,
+    SingleCard
   },
   data () {
     return {
       guessVotingAlert: false,
       guessVotingFailedAlert: false,
       contentLoaded: true,
-      guessIndex: '',
       guess: {
         id: '0',
+        url: 'www.guesser.io/#/search?_id=',
         title: 'Loading...',
         description: 'Loading...',
-        topic: '',
+        topic: 'Crypto',
         creator: '0x00000000000000000000000000000000',
         votes: 0,
         startingDay: '10-10-10',
@@ -136,43 +94,32 @@ export default {
       ethAmountToVote: 0
     }
   },
-  created: function () {
-    let self = this
-
-    GuessHelper.init().then(() => {
-      self.contentLoaded = false
-      self.searchForGuess()
-    }).catch(err => {
-      console.log(err)
-    })
-  },
-  beforeUpdate: function () {
-    let self = this
-
-    self.searchForGuess()
-  },
   methods: {
     searchForGuess () {
       let self = this
 
       self.guessIndex = this.$route.query._id
       this.contentLoaded = false
+      self.guess.id = self.guessIndex
       self.getGuess(self.guessIndex)
       self.getOptions(self.guessIndex)
       self.getOptionsProfits(self.guessIndex)
+    },
+    generateEventUrl () {
+      this.guess.url += this.guess.id
     },
     getGuess (_id) {
       let self = this
 
       GuessHelper.getGuessFront(_id).then((guessFound) => {
-        // console.log(guessFound)
+        // TODO: Solve this mess
+        self.guess.url = window.location.href
 
         self.guess.title = guessFound[0]
         self.guess.description = guessFound[1]
         self.guess.topic = guessFound[2]
-        self.guess.votes = guessFound[4]
-        self.guess.startingDay = this.$moment(guessFound[5]).format('MMMM D, YYYY [at] H[h]')
-        self.guess.finishingDay = this.$moment(guessFound[6]).format('MMMM D, YYYY [at] H[h]')
+        self.guess.startingDay = this.$moment(guessFound[4]).format('MMMM D, YYYY [at] H[h]')
+        self.guess.finishingDay = this.$moment(guessFound[5]).format('MMMM D, YYYY [at] H[h]')
         self.guessExists = true
       }).catch(err => {
         return err
@@ -186,6 +133,7 @@ export default {
         self.guess.option2 = guessOptions[1]
         self.guess.option1votes = guessOptions[2].c[0]
         self.guess.option2votes = guessOptions[3].c[0]
+        self.guess.votes = self.guess.option1votes + self.guess.option2votes
       }).catch(err => {
         self.guessExists = false
         return err
@@ -195,26 +143,28 @@ export default {
       let self = this
 
       GuessHelper.getGuessOptionsProfits(_id).then((optionsAmount) => {
-        self.guess.option1AmountEth = parseInt(optionsAmount[0]) / 10
-        self.guess.option2AmountEth = parseInt(optionsAmount[1]) / 10
-        self.guess.amountEth = parseInt(optionsAmount[0]) / 10 + parseInt(optionsAmount[1]) / 10
+        self.guess.option1AmountEth = parseFloat(optionsAmount[0]).toFixed(4) / 10
+        self.guess.option2AmountEth = parseFloat(optionsAmount[1]).toFixed(4) / 10
+        self.guess.amountEth = parseFloat(optionsAmount[0]).toFixed(4) / 10 +
+          parseFloat(optionsAmount[1]).toFixed(4) / 10
       })
-    },
-    showPaymentModal (_optionVoted) {
-      this.optionVoted = _optionVoted
-      this.$refs.paymentModal.show()
-    },
-    voteGuess () { // Option has to be 1 or 2
-      // let self = this
-      this.$refs.paymentModal.hide()
-      GuessHelper.voteGuess(this.guessIndex, this.optionVoted, this.ethAmountToVote).then(() => {
-        console.log('Transaction pending...')
-        // TODO: Show alert of voting
-        this.guessVotingAlert = true
-      }).catch(err => {
-        this.guessVotingFailedAlert = true
-        return err
-      })
+    }
+  },
+  created: function () {
+    let self = this
+
+    GuessHelper.init().then(() => {
+      self.contentLoaded = false
+      self.searchForGuess()
+    }).catch(err => {
+      console.log(err)
+    })
+  },
+  beforeUpdate: function () {
+    let self = this
+
+    if (!this.guessExists || this.$route.query._id !== this.guess.id) {
+      self.searchForGuess()
     }
   }
 }
