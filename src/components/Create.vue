69e189030ba6e720dc637dcad98afd22b430749c
XPi2 <uxiopineiro@gmail.com>
2018-04-09 21:46:48 +0200

    Duration time added to Create (testing)

--- src/components/Create.vue
+++ src/components/Create.vue
@@ -1,116 +1,125 @@
 <template>
-<div class="justify-content-md-center litle-margin">
-  <!--Alert-->
-  <notifications group="creation"
-                 position="top center"
-                 classes="vue-notification creation"
-                 :max="2"
-                 :speed="500" />
-  
-  <!--Form-->
-  <b-form @submit="onSubmit">
-    <h2 style="margin-bottom: 2%;"> Create an event </h2>
-    <!--Title-->
-    <b-form-group id="titleGroup"
-                  label="Title:"
-                  label-for="titleInput">
-      <b-form-input id="titleInput"
-                    type="text"
-                    maxlength="31"
-                    placeholder="Short and clear name of the event"
-                    v-model="form.title"
-                    required>
-      </b-form-input>
-    </b-form-group>
-    
-    <!--Description-->
-    <b-form-group id="descriptionGroup"
-                  label="Description:"
-                  label-for="descriptionInput"
-                  style="margin-bottom: 0">
-      <b-form-textarea id="descriptionInput"
-                       maxlength="140"
-                       placeholder="Tell people about what's happening in 140 characters"
-                       v-model="form.description"
-                       required>
-      </b-form-textarea>
-      <b-row style="padding-top: 10px" align-h="center">
-        <vue-twitter-counter :current-length="140 - remchar"
-                             safe="#ff66ff"
-                             :danger-at='140'
-                             animate
-                             round>
-        </vue-twitter-counter>
-      </b-row>
-      <!--TODO: Update when change input-->
-      <!--<span> {{ remchar }} characters remaining</span>-->
-    </b-form-group>
-    
-    <!--Topics-->
-    <p class='info-section'>Topic:</p>
-    <div v-if="windowWidth >= 800">
-      <b-form-radio-group id="btnradios2"
-                          buttons
-                          button-variant="outline-primary"
-                          size="sm"
-                          v-model="form.topic"
-                          :options="topics"
-                          name="radioBtnOutline" />
-</b-form-group>
-</div>
-<div v-else>
-  <b-dropdown id="ddown" text="Select a topic" variant="primary" class="m-2">
-    <b-form-radio-group id="btnradios3"
-                        style="width: 100%"
-                        buttons
-                        stacked
-                        button-variant="outline-primary"
-                        v-model="form.topic"
-                        :options="topics"
-                        name="radioBtnOutline" />
-</b-form-group>
-</b-dropdown>
-</div>
-</b-form-group>
+  <div class="justify-content-md-center litle-margin">
+    <!--Alert-->
+    <notifications group="creation"
+                   position="top center"
+                   classes="vue-notification creation"
+                   :max="2"
+                   :speed="500" />
 
-<br>
-<br>
-<!--Outcomes-->
-<p class='info-section'>Outcomes:</p>
-<b-form inline>
-  <label class="sr-only" for="option1Input" >Outcome1</label>
-  <b-input class="mb-2 mr-sm-2 mb-sm-0"
-           id="option1Input"
-           v-model='form.option1'
-           maxlength="31"
-           placeholder="Outocome1"/>
-  <label class="sr-only" for="option2Input">Outcome2</label>
-  <b-input class="mb-2 mr-sm-2 mb-sm-0"
-           id="option2Input"
-           v-model='form.option2'
-           maxlength="31"
-           placeholder="Outcome2"/>
-</b-form>
-<div>
-  <br>
-  <span>Ending date and time: {{updateDate}}</span>
-<br>
-<b-form-slider 
-  :v-model='hourValue'
-  :value='0'
-  :min='1'
-  :max='120'
-  :step='1'
-  v-bind:rangeHighlights='highlights'
-  @change="changeSlider"
-  />
+    <!--Form-->
+    <b-form @submit="onSubmit">
+      <h2 style="margin-bottom: 2%;"> Create an event </h2>
+      <!--Title-->
+      <b-form-group id="titleGroup"
+                    label="Title:"
+                    label-for="titleInput">
+        <b-form-input id="titleInput"
+                      type="text"
+                      maxlength="31"
+                      placeholder="Short and clear name of the event"
+                      v-model="form.title"
+                      required>
+        </b-form-input>
+      </b-form-group>
 
-</div>
-<br>
-<br>
-<b-button type="submit" variant="primary" size='lg'>Create</b-button>
-</b-form>
-</div>
+      <!--Description-->
+      <b-form-group id="descriptionGroup"
+                    label="Description:"
+                    label-for="descriptionInput"
+                    style="margin-bottom: 0">
+        <b-form-textarea id="descriptionInput"
+                         maxlength="140"
+                         placeholder="Tell people about what's happening in 140 characters"
+                         v-model="form.description"
+                         required>
+        </b-form-textarea>
+        <b-row style="padding-top: 10px" align-h="center">
+          <vue-twitter-counter :current-length="140 - remchar"
+                                         safe="#ff66ff"
+                                         :danger-at='140'
+                                         animate
+                                         round>
+          </vue-twitter-counter>
+        </b-row>
+        <!--TODO: Update when change input-->
+        <!--<span> {{ remchar }} characters remaining</span>-->
+      </b-form-group>
+
+      <!--Topics-->
+      <p class='info-section'>Topic:</p>
+      <div v-if="windowWidth >= 800">
+        <b-form-radio-group id="btnradios2"
+                            buttons
+                            button-variant="outline-primary"
+                            size="sm"
+                            v-model="form.topic"
+                            :options="topics"
+                            name="radioBtnOutline" />
+        </b-form-group>
+      </div>
+      <div v-else>
+        <b-dropdown id="ddown" text="Select a topic" variant="primary" class="m-2">
+          <b-form-radio-group id="btnradios3"
+                              style="width: 100%"
+                              buttons
+                              stacked
+                              button-variant="outline-primary"
+                              v-model="form.topic"
+                              :options="topics"
+                              name="radioBtnOutline" />
+          </b-form-group>
+        </b-dropdown>
+      </div>
+    </b-form-group>
+
+    <br>
+    <br>
+    <!--Outcomes-->
+    <p class='info-section'>Outcomes:</p>
+    <b-form inline>
+      <label class="sr-only" for="option1Input" >Outcome1</label>
+      <b-input class="mb-2 mr-sm-2 mb-sm-0"
+               id="option1Input"
+               v-model='form.option1'
+               maxlength="31"
+               placeholder="Outocome1"/>
+        <label class="sr-only" for="option2Input">Outcome2</label>
+        <b-input class="mb-2 mr-sm-2 mb-sm-0"
+                 id="option2Input"
+                 v-model='form.option2'
+                 maxlength="31"
+                 placeholder="Outcome2"/>
+        </b-form>
+        <div>
+          <br>
+          <br>
+          <p class="info-section">Duration of the event (hours):</p>
+          <b-form inline>
+            <b-input class="mb-2 mr-sm-2 mb-sm-0"
+                     id="durationTime"
+                     v-model='form.durationTime'
+                     placeholder="Duration of the event"/>
+            </b-form>
+            <br>
+            <span>Ending date and time: {{updateDate}}</span>
+            <br>
+            <b-form-slider 
+                     :v-model='hourValue'
+                     :value='0'
+                     :min='1'
+                     :max='120'
+                     :step='1'
+                     v-bind:rangeHighlights='highlights'
+                     @change="changeSlider"
+                     />
+
+        </div>
+        <br>
+        <br>
+        <b-button type="submit" variant="primary" size='lg'>Create</b-button>
+            </b-form>
+  </div>
 </template>
 
 <script>
@@ -135,6 +144,7 @@ export default {
         description: '',
         topic: '',
         date: null,
+        durationTime: 1,
         option1: '',
         option2: ''
       },
@@ -171,14 +181,17 @@ export default {
       evt.preventDefault()
       let self = this
 
-      let finalDate = self.$moment().subtract(self.$moment().minute(), 'minutes').add(self.hourValue, 'hours').unix()
-      // console.log(self.$moment().subtract(self.$moment().minute(), 'minutes').add(self.hourValue, 'hours').format('MMMM D, YYYY [at] H[h]'))
+      let finalDate = self.$moment().subtract(self.$moment().minute(), 'minutes').add(self.hourValue, 'hours')
+      var validationDate = self.$moment(finalDate).add(self.form.durationTime, 'hours')
+      console.log(finalDate.format('[Final:] MMMM D, YYYY [at] H[h]'))
+      console.log(validationDate.format('[Validation:] MMMM D, YYYY [at] H[h]'))
       self.show('creation', 'success')
       GuessHelper.setGuessFront(
         this.form.title,
         this.form.description,
         this.form.topic,
-        finalDate,
+        finalDate.unix(),
+        validationDate.unix(),
         this.form.option1,
         this.form.option2).then(() => {
           console.log('Transaction pending...')
@@ -199,7 +212,6 @@ export default {
       var startTime = self.$moment().subtract(self.$moment().minute(), 'minutes')
       self.form.date = startTime.add(self.hourValue, 'hours').format('MMMM D, YYYY [at] H[h]')
       self.updateDate = self.form.date
-      console.log(startTime.add(self.hourValue, 'hours').unix())
     }
   },
   computed: {
@@ -230,40 +242,40 @@ export default {
 
 <style>
 .info-section{
-    margin-bottom: 3px;
+  margin-bottom: 3px;
 }
 .row{
-    margin-left: 2% !important;
-    margin-right: 2% !important;
+  margin-left: 2% !important;
+  margin-right: 2% !important;
 }
 .litle-margin{
-    margin: 0% 10%;
-    padding: 3% 0%;
-    max-width: 800px;
+  margin: 0% 10%;
+  padding: 3% 0%;
+  max-width: 800px;
 }
 .btn-primary.dropdown-toggle:focus {
-    box-shadow: 0 0 0 0.2rem #ff0d73 !important;
+  box-shadow: 0 0 0 0.2rem #ff0d73 !important;
 }
 .creation{
-    margin: 5px;
-    border-radius: 2px;
-    border-left: 0px !important;
+  margin: 5px;
+  border-radius: 2px;
+  border-left: 0px !important;
 }
 
 .d-inline-block {
-    display: inline !important;
+  display: inline !important;
 }
 .slider.slider-horizontal {
-    width: 100% !important;
+  width: 100% !important;
 }
 .slider {
-    width: 100% !important;
+  width: 100% !important;
 }
 .slider-tick-label {
 }
 
 .primary-slider {
-    background: purple;
+  background: purple;
 }
 .secondary-slider {
   background: pink;
