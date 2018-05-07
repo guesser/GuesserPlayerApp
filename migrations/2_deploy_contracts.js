4b75c190c25af98e3d1fda2a8fde4b9decb01405
Carlos Gonzalez <carlosgj94@gmail.com>
2018-05-07 12:36:29 +0200

    Adding the new Payments to the migrations deploy

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -4,6 +4,7 @@ const GuesserCore = artifacts.require("./GuesserCore.sol");
 const GuesserGame = artifacts.require("./GuesserGame.sol");
 const MyGuesses = artifacts.require("./MyGuesses.sol");
 const GuesserInputs = artifacts.require("./GuesserInputs.sol");
+const GuesserPayments = artifacts.require("./GuesserPayments.sol");
 
 // Interfaces
 // const GuesserStorageInterface = artifacts.require("./interface/GuesserStorageInterface.sol")
@@ -15,12 +16,16 @@ module.exports = async function(deployer, network) {
         return deployer.deploy(GuesserGame, GuesserStorage.address).then(() => {
           return deployer.deploy(MyGuesses, GuesserStorage.address).then(() => {
             return deployer.deploy(GuesserInputs, GuesserStorage.address).then(() => {
-              return GuesserStorage.deployed().then(async GuesserStorageInstance => {
-                await GuesserStorageInstance.setOwner(GuesserCore.address);
-                await GuesserStorageInstance.setOwner(GuesserGame.address);
-                await GuesserStorageInstance.setOwner(MyGuesses.address);
-                await GuesserStorageInstance.setOwner(GuesserInputs.address);
-                return deployer;
+              return deployer.deploy(GuesserPayments, GuesserStorage.address).then(() => {
+                return GuesserStorage.deployed().then(async GuesserStorageInstance => {
+                  console.log('Setting the owners to the storage...')
+                  await GuesserStorageInstance.setOwner(GuesserCore.address);
+                  await GuesserStorageInstance.setOwner(GuesserGame.address);
+                  await GuesserStorageInstance.setOwner(MyGuesses.address);
+                  await GuesserStorageInstance.setOwner(GuesserInputs.address);
+                  await GuesserStorageInstance.setOwner(GuesserPayments.address);
+                  return deployer;
+                });
               });
             });
           });
