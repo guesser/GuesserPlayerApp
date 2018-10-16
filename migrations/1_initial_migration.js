b7b6e4734482bce5147a93c2d4054a1c3db4b3ce
carlosgj94 <carlosgj94@gmail.com>
2018-10-16 10:27:34 +0200

    Builded the project after deploying

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,4 +1,5 @@
 var Migrations = artifacts.require("./Migrations.sol");
+const Web3 = require('web3');
 
 module.exports = function(deployer) {
   deployer.deploy(Migrations);
--- truffle.js
+++ truffle.js
@@ -1,17 +1,18 @@
 module.exports = {
-    networks: {
-        development: {
-            host: "localhost",
-            port: 8545,
-            network_id: "*", // Match any network id
-          gas: 6500000
-        },
-        rinkeby: {
-          host: "localhost", // Connect to geth on the specified
-          port: 8545,
-          network_id: '4',
-          gas: 4700000 // Gas limit used for deploys
-        }
+  networks: {
+    development: {
+      host: "localhost",
+      port: 8545,
+      network_id: "*", // Match any network id
+      gas: 6500000
+    },
+    rinkeby: {
+      host: "localhost", // Connect to geth on the specified
+      port: 8545,
+      from: "0x5732CF0275a3Ae210D2caBE874E655297cCe892C",
+      network_id: 4,
+      gas: 7628394 // Gas limit used for deploys
     }
+  }
 };
 
